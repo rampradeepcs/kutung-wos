420c62dbe177606c6184ba947ba5044ac66333f4
RandyRam <itsramkumar7@gmail.com>
2021-01-06 12:32:14 +0530

    mail function added

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,41 +14,41 @@ $(document).ready(function() {
         }
     });
 
-    var validateForm = function() {
-        let inputs = $("#contact-form").find(".form-control");
+    // var validateForm = function() {
+    //     let inputs = $("#contact-form").find(".form-control");
         
-        inputs.each((key, obj) => {
-            let input = document.getElementById($(obj).attr("id"));
+    //     inputs.each((key, obj) => {
+    //         let input = document.getElementById($(obj).attr("id"));
 
-            if($(obj).attr("required") == 'required' && $(obj).val() == '' && !input.checkValidity()) {
-                $(obj).parent(".form-group").addClass("has-error");
-            } else {
-                $(obj).parent(".form-group").removeClass("has-error");
-            }
+    //         if($(obj).attr("required") == 'required' && $(obj).val() == '' && !input.checkValidity()) {
+    //             $(obj).parent(".form-group").addClass("has-error");
+    //         } else {
+    //             $(obj).parent(".form-group").removeClass("has-error");
+    //         }
 
-            if($(obj).attr("id") == 'email') {
-                let ptrn = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
-                if(ptrn.test(input.value)) {
-                    $(obj).parent(".form-group").removeClass("has-error");
-                } else {
-                    $(obj).parent(".form-group").addClass("has-error");
-                }
-            }
-            if($(obj).attr("id") == 'contact') {
-                let num = /^\d{10}$/;
-                if(num.test(input.value)) {
-                    $(obj).parent(".form-group").removeClass("has-error");
-                } else {
-                    $(obj).parent(".form-group").addClass("has-error");
-                }
-            }
-        });
-    };
+    //         if($(obj).attr("id") == 'email') {
+    //             let ptrn = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
+    //             if(ptrn.test(input.value)) {
+    //                 $(obj).parent(".form-group").removeClass("has-error");
+    //             } else {
+    //                 $(obj).parent(".form-group").addClass("has-error");
+    //             }
+    //         }
+    //         if($(obj).attr("id") == 'contact') {
+    //             let num = /^\d{10}$/;
+    //             if(num.test(input.value)) {
+    //                 $(obj).parent(".form-group").removeClass("has-error");
+    //             } else {
+    //                 $(obj).parent(".form-group").addClass("has-error");
+    //             }
+    //         }
+    //     });
+    // };
 
-    $(".contact-submit-btn").on("click", function(e) {        
-        e.preventDefault();
-        validateForm();
-    });
+    // $(".contact-submit-btn").on("click", function(e) {        
+    //     e.preventDefault();
+    //     validateForm();
+    // });
 
     // $(".contact-submit-btn:last-child").on("click", function() {
     //     e.preventDefault();
@@ -59,13 +59,13 @@ $(document).ready(function() {
     //     });
     // });
 
-    $("#contact").on("keydown", function(e) {
-        let val = $(this).val();
-        val.replace(/^\d{10}$/, "");
-        if ((e.which < 48 || e.which > 57)) {
-            e.preventDefault();
-        }
-    });
+    // $("#contact").on("keydown", function(e) {
+    //     let val = $(this).val();
+    //     val.replace(/^\d{10}$/, "");
+    //     if ((e.which < 48 || e.which > 57)) {
+    //         e.preventDefault();
+    //     }
+    // });
 
     $(".cus-select input").on("click", function() {
         if($(this).siblings("ul").hasClass("enable") == false)
--- mail.js
+++ mail.js
@@ -5,26 +5,53 @@
         Ajax Forms
       -------------------------------------------------------------------------------*/
 
-      if ($('.js-form').length) {
-          $('.js-form').each(function(){
-              $(this).validate({
-                  errorClass: 'error wobble-error',
-                  submitHandler: function(form){
-                      $.ajax({
-                          type: "POST",
-                          url:"mail.php",
-                          data: $(form).serialize(),
-                          success: function() {
-                              $('.success-message').show();
-                          },
+    //   if ($('.js-form').length) {
+    //       $('.js-form').each(function(){
+    //           $(this).validate({
+    //               errorClass: 'error',
+    //               submitHandler: function(form){
+    //                   $.ajax({
+    //                       type: "POST",
+    //                       url:"mail.php",
+    //                       data: $(form).serialize(),
+    //                       success: function() {
+    //                           $('.success-message').show();
+    //                       },
   
-                          error: function(){
-                              $('.error-message').show();
-                          }
-                      });
-                  }
-              });
-          });
-      }
+    //                       error: function(){
+    //                           console.log("error");
+    //                           $('.error-message').show();
+    //                       }
+    //                   });
+    //               }
+    //           });
+    //       });
+    //   }
+
+    $("#contact-form").validate({
+        errorClass: 'has-error',
+        errorElement: 'span',
+        highlight: function(element, errorClass, validClass) {
+            $(element).parents("div.form-group").addClass(errorClass);
+        },
+        unhighlight: function(element, errorClass, validClass) {
+            $(element).parents(".has-error").removeClass(errorClass);
+        },
+        submitHandler: function(form){
+            $.ajax({
+                type: "POST",
+                url:"mail.php",
+                data: $(form).serialize(),
+                success: function() {
+                    $('.success-message').show();
+                },
+
+                error: function(){
+                    console.log("error");
+                    $('.error-message').show();
+                }
+            });
+        }
+    });
   
   })(jQuery);

5c40aa3736a52c9a91a75af3bd42c5b458a4b7ef
rampradeepcs <rampradeepcs@gmail.com>
2021-01-12 19:51:33 +0530

    UI

--- assets/js/index.js
+++ assets/js/index.js
@@ -49,7 +49,7 @@ $(document).ready(function () {
   }, 1000);
 
   $(".carousel").carousel({
-    interval: 3000,
+    interval: 5000,
   });
 
   $("#bannerCarousel").on("slide.bs.carousel", function (e) {
